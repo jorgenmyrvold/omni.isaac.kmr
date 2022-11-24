ba37b0258c172d2a8f0c1c149b9d569b7951e4ca
Jørgen Myrvold <myrvoldou@gmail.com>
2022-11-24 16:48:53 +0100

    Started creating ui

--- omni/isaac/kmr/kmr_ros2/kmr_ros2_extension.py
+++ omni/isaac/kmr/kmr_ros2/kmr_ros2_extension.py
@@ -2,7 +2,7 @@ import os
 import omni.ui as ui
 from omni.isaac.examples.base_sample import BaseSampleExtension
 from omni.isaac.kmr.kmr_ros2 import KMRROS2
-from omni.isaac.ui.ui_utils import get_style, dropdown_builder
+from omni.isaac.ui.ui_utils import get_style, dropdown_builder, btn_builder
 
 
 class KMRROS2Extension(BaseSampleExtension):
@@ -19,22 +19,50 @@ class KMRROS2Extension(BaseSampleExtension):
             sample=KMRROS2(),
         )
         self.environment = "Grid"
+        self.robot = "KMR"
+        
+        self.config_ui_elements = {}
+        frame = self.get_frame(index=0)
+        self._build_config_ui(frame)
+
         return
 
-    def _build_config_ui(self):
-        frame = ui.CollapsableFrame(
-            title="Import Options",
-            height=0,
-            collapsed=False,
-            style=get_style(),
-            style_type_name_override="CollapsableFrame",
-            horizontal_scrollbar_policy=ui.ScrollBarPolicy.SCROLLBAR_AS_NEEDED,
-            vertical_scrollbar_policy=ui.ScrollBarPolicy.SCROLLBAR_ALWAYS_ON,
-        )
+
+    def set_environment(self, env):
+        self.environment=env
+    
+    def set_robot(self, robot):
+        self.robot = robot
+   
+
+    def _build_config_ui(self, frame):
         with frame:
             with ui.VStack(style=get_style(), spacing=5, height=0):
+                frame.title = "Configuration"
+                frame.visible = True
+
                 dropdown_builder(
                     "Environment",
                     items=["Grid", "Simple warehouse", "Warehouse"],
-                    default_val=1,
+                    default_val=0,
+                    on_clicked_fn=lambda env: self.set_environment(env),
+                    tooltip="Select environment",
                 )
+
+                dropdown_builder(
+                    "Robot",
+                    items=["KMR", "O3dyn", "Honomic Dummy"],
+                    default_val=0,
+                    on_clicked_fn=lambda robot: self.set_robot(robot),
+                    tooltip="Select environment",
+                )
+
+                dict = {
+                    "label": "Print env+robot",
+                    "type": "button",
+                    "text": "Print",
+                    "tooltip": "Print",
+                    "on_clicked_fn": lambda: print(self.environment, self.robot),
+                }
+                self.config_ui_elements["Print env+robot"] = btn_builder(**dict)
+                self.config_ui_elements["Print env+robot"].enabled = True
