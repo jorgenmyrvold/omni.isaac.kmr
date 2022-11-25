2ebf6d666aa46aaab59c68bdcde3d1b791185d82
Jørgen Myrvold <myrvoldou@gmail.com>
2022-11-25 16:27:59 +0100

    Finished setup scene, o3dyn. Not finished dataloger

--- omni/isaac/kmr/kmr_ros2/kmr_ros2_extension.py
+++ omni/isaac/kmr/kmr_ros2/kmr_ros2_extension.py
@@ -2,7 +2,7 @@ import os
 import omni.ui as ui
 from omni.isaac.examples.base_sample import BaseSampleExtension
 from omni.isaac.kmr.kmr_ros2 import KMRROS2
-from omni.isaac.ui.ui_utils import get_style, dropdown_builder, btn_builder
+from omni.isaac.ui.ui_utils import get_style, dropdown_builder, btn_builder, str_builder, state_btn_builder
 
 
 class KMRROS2Extension(BaseSampleExtension):
@@ -17,23 +17,31 @@ class KMRROS2Extension(BaseSampleExtension):
             overview="KMR ROS2 extension",
             file_path=os.path.abspath(__file__),
             sample=KMRROS2(),
+            number_of_extra_frames=2,
         )
-        self.environment = "Grid"
+        self.environment = "Grid/default_environment"
         self.robot = "KMR"
         
-        self.config_ui_elements = {}
+        self.ui_elements = {}
         frame = self.get_frame(index=0)
         self._build_config_ui(frame)
-
+        frame = self.get_frame(index=1)
+        self.build_data_logging_ui(frame)
         return
 
 
-    def set_environment(self, env):
-        self.environment=env
+    # def set_environment(self, env):
+    #     # Used due to trouble with lambda functions in the dropdown_builder
+    #     self.environment=env
     
-    def set_robot(self, robot):
-        self.robot = robot
+    # def set_robot(self, robot):
+    #     # Used due to trouble with lambda functions in the dropdown_builder
+    #     self.robot = robot
    
+    # def _on_set_configuration_button_event(self):
+    #     self._sample._on_set_config_event(self.robot, self.environment)
+    #     return
+
 
     def _build_config_ui(self, frame):
         with frame:
@@ -41,28 +49,91 @@ class KMRROS2Extension(BaseSampleExtension):
                 frame.title = "Configuration"
                 frame.visible = True
 
-                dropdown_builder(
-                    "Environment",
-                    items=["Grid", "Simple warehouse", "Warehouse"],
+                self.ui_elements["Environment"] = dropdown_builder(
+                    label="Environment",
+                    items=["Grid/default_environment", "Simple_Warehouse/warehouse_with_forklifts", "Simple_Warehouse/warehouse_multiple_shelves"],
                     default_val=0,
-                    on_clicked_fn=lambda env: self.set_environment(env),
+                    on_clicked_fn=lambda env: self.sample.on_select_environment(env),
                     tooltip="Select environment",
                 )
 
-                dropdown_builder(
-                    "Robot",
-                    items=["KMR", "O3dyn", "Honomic Dummy"],
+                self.ui_elements["Robot"] = dropdown_builder(
+                    label="Robot",
+                    items=["KMR", "O3dyn", "Holonomic Dummy"],
                     default_val=0,
-                    on_clicked_fn=lambda robot: self.set_robot(robot),
-                    tooltip="Select environment",
+                    on_clicked_fn=lambda robot: self.sample.on_select_robot(robot),
+                    tooltip="Select Robot",
                 )
 
                 dict = {
-                    "label": "Print env+robot",
+                    "label": "Change config",
                     "type": "button",
-                    "text": "Print",
-                    "tooltip": "Print",
-                    "on_clicked_fn": lambda: print(self.environment, self.robot),
+                    "text": "Change config",
+                    "tooltip": "Change configuration",
+                    "on_clicked_fn": self.sample._on_change_config_event,
                 }
-                self.config_ui_elements["Print env+robot"] = btn_builder(**dict)
-                self.config_ui_elements["Print env+robot"].enabled = True
+                self.ui_elements["Change config"] = btn_builder(**dict)
+                self.ui_elements["Change config"].enabled = True
+
+
+    def _on_logging_button_event(self, val):
+        self.sample._on_logging_event(val)
+        self.ui_elements["Save Data"].enabled = True
+        return
+    
+    def _on_save_data_button_event(self):
+        self.sample._on_save_data_event(self.ui_elements["Output Directory"].get_value_as_string())
+        return
+
+    def build_data_logging_ui(self, frame):
+        with frame:
+            with ui.VStack(spacing=5):
+                frame.title = "Data Logging"
+                frame.visible = True
+                dict = {
+                    "label": "Output Directory",
+                    "type": "stringfield",
+                    "default_val": os.path.join(os.getcwd(), "output_data.json"),
+                    "tooltip": "Output Directory",
+                    "on_clicked_fn": None,
+                    "use_folder_picker": False,
+                    "read_only": False,
+                }
+                self.ui_elements["Output Directory"] = str_builder(**dict)
+
+                dict = {
+                    "label": "Start Logging",
+                    "type": "button",
+                    "a_text": "START",
+                    "b_text": "PAUSE",
+                    "tooltip": "Start Logging",
+                    "on_clicked_fn": self._on_logging_button_event,
+                }
+                self.ui_elements["Start Logging"] = state_btn_builder(**dict)
+                self.ui_elements["Start Logging"].enabled = False
+
+                dict = {
+                    "label": "Save Data",
+                    "type": "button",
+                    "text": "Save Data",
+                    "tooltip": "Save Data",
+                    "on_clicked_fn": self._on_save_data_button_event,
+                }
+                self.ui_elements["Save Data"] = btn_builder(**dict)
+                self.ui_elements["Save Data"].enabled = False
+
+                dict = {
+                    "label": "Print pos",
+                    "type": "button",
+                    "text": "Print pos",
+                    "on_clicked_fn": self.sample.print_pos_scene,
+                }
+                self.ui_elements["Print pos"] = btn_builder(**dict)
+                # self.ui_elements["Print pos"].enabled = True
+
+    def post_load_button_event(self):
+        self.ui_elements["Start Logging"].enabled = True
+        self.ui_elements["Save Data"].enabled = False
+    
+    def post_reset_button_event(self):
+        return
